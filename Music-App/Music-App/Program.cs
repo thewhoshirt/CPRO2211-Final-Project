3c3da61dcf6b3f4faccee67154d670305c859355
janelle-and <janelle.andrews@rdpolytech.ca>
2025-11-30 19:49:29 -0700

    changed main controller

--- Music-App/Music-App/Program.cs
+++ Music-App/Music-App/Program.cs
@@ -19,6 +19,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Musics}/{action=Index}/{id?}");
 
 app.Run();
