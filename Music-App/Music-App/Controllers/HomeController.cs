bd58c807834eccdba087904b01d3d4d7f989ecf9
janelle-and <janelle.andrews@rdpolytech.ca>
2025-11-30 19:36:49 -0700

    scaffolding

--- Music-App/Music-App/Controllers/HomeController.cs
+++ Music-App/Music-App/Controllers/HomeController.cs
@@ -6,9 +6,17 @@ namespace Music_App.Controllers
 {
     public class HomeController : Controller
     {
+        private MusicContext context { get; set; }
+
+        public HomeController(MusicContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var music = context.Musics.OrderBy(m => m.TrackId).ToList();
+            return View(music);
         }
     }
 }
---
+++ Music-App/Music-App/Controllers/MusicsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Music_App.Models;
+
+namespace Music_App.Controllers
+{
+    public class MusicsController : Controller
+    {
+        private readonly MusicContext _context;
+
+        public MusicsController(MusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Musics
+        public async Task<IActionResult> Index()
+        {
+              return _context.Musics != null ? 
+                          View(await _context.Musics.ToListAsync()) :
+                          Problem("Entity set 'MusicContext.Musics'  is null.");
+        }
+
+        // GET: Musics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Musics == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Musics
+                .FirstOrDefaultAsync(m => m.TrackId == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // GET: Musics/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Musics/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TrackId,TrackFile,TrackTitle,TrackArtist,TrackLength")] Music music)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(music);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Musics == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Musics.FindAsync(id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            return View(music);
+        }
+
+        // POST: Musics/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TrackId,TrackFile,TrackTitle,TrackArtist,TrackLength")] Music music)
+        {
+            if (id != music.TrackId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(music);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MusicExists(music.TrackId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(music);
+        }
+
+        // GET: Musics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Musics == null)
+            {
+                return NotFound();
+            }
+
+            var music = await _context.Musics
+                .FirstOrDefaultAsync(m => m.TrackId == id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+
+            return View(music);
+        }
+
+        // POST: Musics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Musics == null)
+            {
+                return Problem("Entity set 'MusicContext.Musics'  is null.");
+            }
+            var music = await _context.Musics.FindAsync(id);
+            if (music != null)
+            {
+                _context.Musics.Remove(music);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MusicExists(int id)
+        {
+          return (_context.Musics?.Any(e => e.TrackId == id)).GetValueOrDefault();
+        }
+    }
+}
