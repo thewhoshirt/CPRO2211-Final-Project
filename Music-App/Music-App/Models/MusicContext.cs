a52e266ceada2676e8d0394d7b2be6ecb2ce4abd
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-05 16:29:12 -0700

    musicContext comments

--- Music-App/Music-App/Models/MusicContext.cs
+++ Music-App/Music-App/Models/MusicContext.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Music_App.Models;
-
+﻿using Microsoft.EntityFrameworkCore; // Import EntityFrameworkCore so we can use MVC operations
+/*
+ * Add the database context to the models namespace so it can be accessed any time the model is accessed
+ */
 namespace Music_App.Models
 {
+    /*
+     * Create the context
+     */
     public class MusicContext : DbContext
     {
-        public MusicContext(DbContextOptions<MusicContext> options)
-            : base(options)
-        {
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options) {
         }
+        /*
+         * Getters and setters for the playlist and music entities
+         */
         public DbSet<Playlist> Playlists { get; set; }
         public DbSet<Music> Musics { get; set; }
-
+        /*
+         * Ensure that the relationship between the models is valid when creating the database
+         */
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Playlist>()
