04a57730b5e2d1083dc33745ae4cd783d512489f
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-12-05 15:53:00 -0700

    program.cs comments

--- Music-App/Music-App/Program.cs
+++ Music-App/Music-App/Program.cs
@@ -1,8 +1,14 @@
+/*
+ * Import EntityFrameworkCore so we can use MVC functions
+ * and import the model(s) so that the program can create/use the database
+ */
 using Microsoft.EntityFrameworkCore;
 using Music_App.Models;
-
+/*
+ * Create the builder and configure CORS so multiple pages can be used
+ * (this was in preparation for the url streaming but we didn't get that far)
+ */
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -12,24 +18,26 @@ builder.Services.AddCors(options =>
             .AllowAnyHeader();
     });
 });
-
-builder.Services.AddControllers();
-// Add services to the container.
+/*
+ * Add services to the container.
+ */
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<MusicContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MusicContext")));
-
+builder.Services.AddControllers();
+/*
+ * Create the app and configure all of the dependencies and routing
+ */
 var app = builder.Build();
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseCors("AllowAll");
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
+/*
+ * Set the default route to be the index page of the Musics controller
+ */
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Musics}/{action=Index}/{id?}");
-
-
-app.Run();
+app.Run(); // run the app
